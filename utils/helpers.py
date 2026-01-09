60b24a18a1226ea7dabc47cab2982a1c8cecbd34
Leolory <2153576070@qq.com>
2026-01-09 11:32:22 +0800

    new function and hot fix

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,5 @@
+import re
+from typing import List
 def generate_all_prefixes(s: str) -> List[str]:
     """生成字符串的所有前缀（如"138" → ["1", "13", "138"]）"""
     return [s[:i+1] for i in range(len(s))] if s else []
